f1718c4fc605f3ad94cd1f3c06a95edbb5a67b4f
Melissa Nakasato <melissanakasato@gmail.com>
2026-08-18 09:32:47 -0300

    feat(ap01-git): implementa o menu e a soma

---
+++ menu.py
@@ -0,0 +1,20 @@
+import calculadora
+def menu():
+    print("Escolha uma operação:")
+    print("1. Somar")
+    print("2. Subtrair")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    print("0. Sair")
+
+    calculo = int(input("Digite o número da operação desejada: "))
+    return calculo
+
+calculo = menu()
+a = float(input("Digite o primeiro número: "))
+b = float(input("Digite o segundo número: "))
+
+if calculo == 1:
+    print("Você escolheu somar.")
+    resultado = calculadora.somar(a, b)
+    print('O resultado da soma é:', resultado)
